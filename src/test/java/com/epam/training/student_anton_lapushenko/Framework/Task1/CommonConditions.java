bc5636b69ed8f7714f68e1eb6f937340b5c83c58
mccreed13 <a.lapushenko13@gmail.com>
2024-05-11 01:59:57 +0300

    Added ScreenshotWatcher.

--- src/test/java/com/epam/training/student_anton_lapushenko/Framework/Task1/CommonConditions.java
+++ src/test/java/com/epam/training/student_anton_lapushenko/Framework/Task1/CommonConditions.java
@@ -1,12 +1,17 @@
 package com.epam.training.student_anton_lapushenko.Framework.Task1;
 
 import com.epam.training.student_anton_lapushenko.Framework.Task1.driver.DriverSingleton;
+import com.epam.training.student_anton_lapushenko.Framework.Task1.util.ScreenshotWatcher;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.openqa.selenium.WebDriver;
 
 public class CommonConditions {
     protected WebDriver driver;
+
+    @RegisterExtension
+    ScreenshotWatcher screenshotWatcher = new ScreenshotWatcher("target/screenshots");
 
     @BeforeEach
     public void setUp() {
